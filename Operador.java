63197e6b2bd072ef6597aa419160e6e6eb6954b2
unknown <dorenalto@gmail.com>
2018-10-27 18:43:07 -0300

    JAVA

---
+++ Operador.java
@@ -0,0 +1,29 @@
+/**
+*Apresentar os conceitos de Operadores.
+*@author Dorenalto Mangueira Couto
+
+*/
+
+public class Operador{
+
+public static void main (String[]args) {
+	
+	// 2 + 3 operador binario.
+	// 2 operador unario.
+	// true ? "sim" : "não" operador ternario
+	// Sring x= "2"+"3"; concatenação resultado = 23.
+	// int x = 2+3;
+		int x = 6;
+	//	int y = ++x; pré incremento.
+	int y = x++;// pós incremento.
+	
+	//double y = 7 - 4 + 3 * 2; primeiro vai  calcular multiplicação regra matematica.
+	
+	//double y = 7 % 2; assim você vera o resto da divisão. Pode ser usada pelo programa para achar números pares ou ímpar, por exemplo;
+	System.out.println (x);
+	System.out.println (y);
+	
+   }
+}
+
+ 
---
+++ Operadores.java
@@ -0,0 +1,55 @@
+/**
+*Apresentar os conceitos de Operadores.
+*@author Dorenalto Mangueira Couto
+
+*/
+
+public class Operadores{
+
+public static void main (String[]args) {
+	
+	// 2 + 3 operador binario.
+	// -2 operador unario.
+	//true ? "sim" : "não" operador ternario
+	// Sring x= "2"+"3"; concatenação resultado = 23.
+	int x = 2+3;
+	System.out.println (x);
+	
+	double y = 7 - 4 + 3 * 2;
+	System.out.println (y);
+
+
+
+
+/*
+Operadores          Finalidade
+-----------------------------------------------------------------------------------------
++,-,*,/,%			Soma, subtrai, multiplica, divide, resto de divisão;
++					Concatena strings;
++,-					Operador unario mais e menos;
+<,>,<=,>=			Comparar, menor que, maior que, menor igual que, maior igual que;
+++,--				Pré ou pós incremento, pré ou pós decrementar;
+==,!=				Comparar se é igual ou diferente;
+&&, || 				Concatenação lógica e, comcatenação lógica ou;
+.					Acessar as propriedades de um objeto;
+[]					Indexar arrays;
+()					Chamar método;
+?:					Ternario retorna booleano em uma condição;
+<<,>>,>>>			Operar bit numérico;
+~					Complemento unário sobre bit;
+!					Complemento lógico;
+&,|,^				Operadores bit And, or e  Xor;
+,					Separara valores;
+=					Atribui valor;
++=,-=,*=,/=			Atribuir valores somando, subritaindo, multiplicando e dividindo;
+<<=,>>=,>>>=		Operador de byte (unário);
+&=,|=,^=			Operador de bytes and, or e Xor (unário);
+instanceof			Verifica o tipo de objeto;
+new					Cria objeto;
+(tipo)				Coerção unária. 		
+*/
+
+
+
+}
+}
